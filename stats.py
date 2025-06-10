808df0e34069a2777ce97c6e130d95538a55f3b5
Michael Friderich <michael@frideri.ch>
2025-06-10 22:08:59 +0200

    add arguments support via CLI

--- stats.py
+++ stats.py
@@ -1,18 +1,23 @@
-def get_word_count (text):
-    count = 0
+def get_num_words(text):
     words = text.split()
-    for word in words:
-        count += 1
-    return count
+    return len(words)
 
-def get_character_count (text):
-    words_count = {}
-    lower_words = text.lower()
-    characters = list(lower_words)
-
-    for characters in lower_words:
-        if characters in words_count:
-            words_count[characters] += 1
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
         else:
-            words_count[characters] = 1
-    return words_count
+            chars[lowered] = 1
+    return chars
+
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
